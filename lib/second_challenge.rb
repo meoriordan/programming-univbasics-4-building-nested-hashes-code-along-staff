1298601f2c530f6dfa69a5a41f5ee4092d1b73f9
Elizabeth ORiordan <elizabeth.oriordan@flatironschool.com>
2020-01-13 01:18:19 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -2,10 +2,15 @@ def second_challenge
   epic_tragedy = {
     :montague => {
       :patriarch => {},
-      :matriarch
+      :matriarch => {},
+      :hero => {},
+      :hero_friends => []
       }, 
     :capulet => {
-      #your key/value pairs here!
+      :patriarch => {},
+      :matriarch => {},
+      :heroine => {},
+      :heorine_friends => []
       }}
 
   
